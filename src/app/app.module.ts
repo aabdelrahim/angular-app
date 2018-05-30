efb3a655899dd5e8bff873afe30f480aefeb0334
Akram Abdelrahim <zapemz@gmail.com>
2018-05-29 22:46:11 -0600

    fix serving errors/ add firebase library to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { MaterializeModule } from 'angular2-materialize';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { environment } from './../environments/environment';
+import { MaterializeModule } from 'angular2-materialize';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase, 'weather-app'),
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDLtOetykfj06VBM41_wNTwPrp7LVVKndc',
+    authDomain: 'test-angular-app-0-1-0.firebaseapp.com',
+    databaseURL: 'https://test-angular-app-0-1-0.firebaseio.com',
+    projectId: 'test-angular-app-0-1-0',
+    storageBucket: 'test-angular-app-0-1-0.appspot.com',
+    messagingSenderId: '67014288449'
+  }
 };
 
 /*
